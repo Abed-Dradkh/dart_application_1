b9fd8606c6b3ce599f8ed03ad30b2c8207d09379
Abed-Dradkh <abed.dradkh@gmail.com>
2024-02-19 21:24:50 +0300

    main

--- lib/emtithal folder/practice.dart
+++ lib/emtithal folder/practice.dart
@@ -1,3 +1,13 @@
 void practice() {
   //here to practice
+  //^ Emtithal => EMTITHAL
+  //^ Emtithal@gmail.com => Emtithal => EMTITHAL
+  //^ Emtithal@gmail.com => Emtithal@hotmail.org
+
+  //^ salary + bouns + experianse
+  //^ bouns based on experianse < 5 get 10% of salary
+  //^ else 20%
+  //^ print salary before & after + bouns
+  //^ calculate for next 3 year
+  //^ 3 10% => 4 10% => 5 20% => 6 20%
 }
