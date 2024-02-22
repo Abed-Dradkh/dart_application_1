bc059a4a2ca9e3617e0c79c8b05928ce24c7fc88
Abed-Dradkh <abed.dradkh@gmail.com>
2024-02-22 18:30:14 +0300

    main

--- lib/emtithal folder/practice3.dart
+++ lib/emtithal folder/practice3.dart
@@ -3,4 +3,5 @@ void practice3() {
   //^ [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
   //^ print indexes that used in your name
   //^ print your name "Emtithal"
+  //! don't use add or insert or fill
 }
