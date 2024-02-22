98e959f9001c71def12afc3dacbfa1146b8c8093
Abed-Dradkh <abed.dradkh@gmail.com>
2024-02-22 18:35:17 +0300

    main

--- lib/emtithal folder/practice3.dart
+++ lib/emtithal folder/practice3.dart
@@ -2,6 +2,7 @@ void practice3() {
   //^ make a list with all letters
   //^ [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
   //^ create list of indexes that used in your name and save them in list
+  //^ for loop to access the value from indexes list
   //^ print your name "Emtithal"
   //^ listname[index] => get value
 }
