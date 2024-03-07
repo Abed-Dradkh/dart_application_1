ae976d57890d54580b235b5f0cca23a46cebe53b
Abed-Dradkh <abed.dradkh@gmail.com>
2024-03-07 19:00:04 +0300

    main

---
+++ lib/emtithal folder/practice13.dart
@@ -0,0 +1,9 @@
+void practice13() {
+  // Write a program (function) that takes a list and returns a new
+  // list that contains all the elements of the first list minus
+  // all the duplicates.
+
+  List<String> names = ['emtithal', 'cat', 'dog', 'emtithal', 'cat'];
+  List<String> newList = [];
+  List<String> list = [];
+}
