e32d64b5260c00da1ef8a4a4418e56752f4c4a9a
Emtithal <emtithalaljabri20@gmail.com>
2024-03-11 19:35:28 +0400

    main

--- bin/main.dart
+++ bin/main.dart
@@ -7,6 +7,7 @@ import 'package:dart_application_1/emtithal%20folder/practice10.dart';
 import 'package:dart_application_1/emtithal%20folder/practice12.dart';
 import 'package:dart_application_1/emtithal%20folder/practice13.dart';
 import 'package:dart_application_1/emtithal%20folder/practice14.dart';
+import 'package:dart_application_1/emtithal%20folder/practice15.dart';
 import 'package:dart_application_1/emtithal%20folder/practice3.dart';
 import 'package:dart_application_1/emtithal%20folder/practice4.dart';
 import 'package:dart_application_1/emtithal%20folder/practice5.dart';
@@ -123,7 +124,8 @@ void main(List<String> arguments) {
   // practice9();
   // practice10();
   // practice12();
-  practice14();
+  // practice14();
+  practice15();
 
   // day1();
   // day2();
--- lib/emtithal folder/practice15.dart
+++ lib/emtithal folder/practice15.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void practice15() {
   //Write a code to ask user for input(size of list)
   //إطلبي من المستخدم إدخال عدة ارقام بناءا على حجم الليست المدخل سابقا
@@ -10,4 +12,49 @@ void practice15() {
 
   //& avarage => sum of all numbers / count
   //& avarage => out of loop ==> when calcualte sum of all number then calcualte avarage
+
+  stdout.write('Enter a number:');
+  String number = stdin.readLineSync() ?? '';
+  int num = int.parse(number);
+
+  List<int> list = [];
+  int max = 0;
+  int min = 0;
+  int sum = 0;
+  double average = 0;
+
+  stdout.writeln('Enter a numbers:');
+
+  for (var i = 0; i < num; i++) {
+    String numbers = stdin.readLineSync() ?? '';
+    int numb = int.parse(numbers);
+    list.add(numb);
+  }
+  print(list);
+  max = list.first;
+  min = list.first;
+
+  for (var i in list) {
+    if (i > max) {
+      max = i;
+    }
+    if (i < min) {
+      min = i;
+    }
+
+    sum = sum + i;
+  }
+  average = sum / list.length;
+  print(max);
+  print(min);
+  print(average);
 }
+
+  // maximum
+  // ...>2
+  // max=..
+  //  ...<2
+  // min=..
+
+  // sum
+//  مجموع الثيم ع عددها
